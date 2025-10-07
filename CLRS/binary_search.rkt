663c281917e10223fa64a317eee6d9f39b96fc4f
Samuel Oktavianus <samueloctavianus97@gmail.com>
2025-10-07 19:35:59 +0700

    update

--- CLRS/binary_search.rkt
+++ CLRS/binary_search.rkt
@@ -3,12 +3,16 @@
 ; List-of-X Number Number Number -> Number
 ; perform a binary search
 (define (binary-search list target start-index end-index)
-  (let ((mid-index (floor (/ (+ start-index end-index) 2))))
-    (cond []
-          []
-          [])
+  (let* ((mid-index (floor (/ (+ start-index end-index) 2)))
+        (mid-element (list-ref list mid-index))
+        )
+    (cond [(> start-index end-index) '()] ;target not found
+          [(eq? mid-element target) mid-element] ;target found
+          [(< target mid-element) (binary-search list target start-index (- mid-index 1))]
+          [else (binary-search list target (+ mid-index 1) end-index)]
+          )
     )
   )
 (define sample-list '(1 5 9 11 16 ))
-(binary-search sample-list 11 0 (- (length sample-list) 1) )
+(binary-search sample-list 16 0 (- (length sample-list) 1) )
 (length sample-list)
