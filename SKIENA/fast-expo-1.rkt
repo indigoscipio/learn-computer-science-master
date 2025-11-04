095a4c8d447fe3d20c40d9d3857abb48d2f0b556
Samuel Oktavianus <samueloctavianus97@gmail.com>
2025-11-04 21:05:58 +0700

    fast expo

--- SKIENA/fast-expo-1.rkt
+++ SKIENA/fast-expo-1.rkt
@@ -5,8 +5,8 @@
 ; Number Number -> Number
 (define (fast-exp a n)
   (cond [(= n 0) 1]
-        [(even? n) (fast-exp a (/ n 2)) ] ; even case
-        [else (* ...)] ;odd case
+        [(even? n) (sqr (fast-exp a (quotient n 2))) ] ; even case
+        [else (* a (sqr (fast-exp a (quotient (- n 1) 2) ))) ] ;odd case
         )
   
   )
@@ -14,3 +14,17 @@
 (fast-exp 2 0) ; return 1
 (fast-exp 1 3) ; return 1
 (fast-exp 2 3) ;return 8
+
+
+; version 2
+(define (fast-exp2 a n)
+  (cond [(< n 0) (fast-exp2 (quotient 1 a) (- n) )]
+        [(= n 0) 1 ]
+        [(even? n) (fast-exp2 (sqr a) (quotient n 2) )]
+        [else (* a (fast-exp2 (sqr a) (quotient (- n 1) 2) ))]
+        )
+  )
+(fast-exp2 2 2) ;return 4
+(fast-exp2 2 0) ; return 1
+(fast-exp2 1 3) ; return 1
+(fast-exp2 2 3) ;return 8
