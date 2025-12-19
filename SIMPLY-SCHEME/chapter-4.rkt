9668d24d8aee9c530b88a5b45fcf0598b0b60e54
Samuel Oktavianus <samueloctavianus97@gmail.com>
2025-12-19 21:06:13 +0700

    update

---
+++ SIMPLY-SCHEME/chapter-4.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+;4.7 Write a procedure that computes the absolute value of its argument
+;by finding the square root of the square of the argument
+(define (abs n)
+  (sqrt (sqr n))
+  )
+
+
+l4.8
