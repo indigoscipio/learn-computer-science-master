4bde98e530af7085d5045a4b51041b76b56ff7d5
Samuel Oktavianus <samueloctavianus97@gmail.com>
2026-01-20 21:05:06 +0700

    update

--- BIRD-FP/exercise-4-4-1.rkt
+++ BIRD-FP/exercise-4-4-1.rkt
@@ -1,2 +1,118 @@
 #lang racket
+(require 2htdp/image)
 
+#|
+state -- (direction, pen, point) 
+direction -- num 
+pen -- bool 
+point -- (num,num) 
+command -- state -+ state
+
+direction
+north 0
+east 1
+south 2
+west 3
+
+move .. command 
+move (O,p,(x,y)) = (O,p,(x - l,y)) 
+move (l,p, (x, y)) = (l,p,(x,y + 1)) 
+move (2,p, (x, y)) = (2,p,(x + 1,y)) 
+move (3,p, (x,y)) = (3,p,(x,y-1)) 
+
+; turtle takes a command
+; and returns a state
+turtle :: [command] -> [state]
+turtle = scan applyto (0, false, (0,0))
+applyto x f = f x
+
+display:: [command] -> [char]
+display = layout . picture . trail . turtle
+
+|#
+
+; =====================================
+
+;; Directions: 0 (N), 1 (E), 2 (S), 3 (W)
+;; Pen: #t (down), #f (up)
+;; Point: (cons x y)
+(struct turtle-state (dir pen point) #:transparent)
+(define initial-state (turtle-state 0 #f (cons 0 0)))
+(define state1 (turtle-state 0 #f (cons 8 0)))
+
+;-------
+
+; COMMANDS
+; MOVE
+; turtlestate -> turtlestate
+(define (move state)
+  (let* ((dir (turtle-state-dir state))
+        (pen (turtle-state-pen state))
+        (point (turtle-state-point state))
+        )
+    (cond [(eq? dir 0) (turtle-state dir pen (cons (- (car point) 1) (cdr point)))] ; go up 1 pt
+          [(eq? dir 1) (turtle-state dir pen (cons (car point) (+ 1 (cdr point))))] ; go right
+          [(eq? dir 2) (turtle-state dir pen (cons (+ (car point) 1) (cdr point))) ]  ; go down
+          [else (turtle-state dir pen (cons (car point) (- (cdr point) 1))) ] ; go left
+          )
+    )
+  )
+(move state1)
+
+; RIGHT/LEFT
+; turtlestate->turtlestate
+(define (right state)
+    (let* ((dir (turtle-state-dir state))
+        (pen (turtle-state-pen state))
+        (point (turtle-state-point state))
+        )
+    (turtle-state (modulo (+ 1 dir) 4) pen point )
+    )
+  )
+
+(define (left state)
+    (let* ((dir (turtle-state-dir state))
+        (pen (turtle-state-pen state))
+        (point (turtle-state-point state))
+        )
+    (turtle-state (modulo (- dir 1) 4) pen point )
+    )
+  )
+(left state1)
+
+; UP/DOWN
+(define (up state)
+    (let* ((dir (turtle-state-dir state))
+        (pen (turtle-state-pen state))
+        (point (turtle-state-point state))
+        )
+    (turtle-state dir #t point)
+    )
+  )
+
+(define (down state)
+    (let* ((dir (turtle-state-dir state))
+        (pen (turtle-state-pen state))
+        (point (turtle-state-point state))
+        )
+    (turtle-state dir #f point)
+    )
+  )
+(up state1)
+
+
+; 4.4.2
+; Define a function (block k) which causes the turtle to trace a solid square of side k.
+
+;repeat:: number command -> list-of-commands
+(define (repeat n cmd)
+  (cond [(zero? n) '()] ;stop applying
+        [else (cons cmd (repeat (- n 1) cmd)) ])
+  )
+(repeat 3 down) ; move the pen down 3 pts
+
+
+; number -> list-of-commands
+(define (block k)
+ (move k)
+  )
--- SIMPLY-SCHEME/exercise-15.rkt
+++ SIMPLY-SCHEME/exercise-15.rkt
@@ -1,7 +1,10 @@
 #lang simply-scheme
 
 (define (drop-once x xs)
-  0
+  (cond [(empty? xs) xs]
+        [(equal? (first xs) x) (bf xs)]
+        [else (se (first xs) (drop-once x (bf xs)))]
+        )
   )
 (drop-once 3 '(1 2 3 4 5 3))
 
@@ -26,7 +29,119 @@
 ; selection sort (sort of)
 ; ss-sort:: list-of-number -> list-of-number
 (define (ss-sort xs)
-  0
-  ;temprary
+  (cond [(empty? xs) '()]
+        [else (se (find-smallest xs) (ss-sort (drop-once (find-smallest xs) xs ))) ])
   )
 (ss-sort '(i wanna be your man))
+
+; ==================
+
+;take
+(define (take n sent)
+  (cond [(empty? sent) '()]
+        [(zero? n) '()]
+        [else (se (first sent) (take (- n 1) (bf sent)))]
+        )
+  )
+(take 3 '(h e l l o))
+
+;drop
+(define (drop n sent)
+  (cond [(empty? sent) sent]
+        [(zero? n) sent]
+        [else (drop (- n 1) (bf sent))]
+        )
+  )
+
+
+; merge:: sentence sentence -> sentence
+(define (merge left right)
+ (cond [(empty? left) right]
+       [(empty? right) left]
+       [(before? (first left) (first right)) (se (first left) (merge (bf left) right)) ] ;but left
+       [else (se (first right) (merge left (bf right)))]
+       )
+  )
+(merge '(1 4 8) '(2 5 6 10))
+
+
+; merge sort
+(define (mergesort sent)
+  (let* ((mid (floor (/ (count sent) 2)))
+        (left-half (take mid sent))
+        (right-half (drop mid sent))
+        )
+    (cond [(<= (count sent) 1) sent]
+          [(merge (mergesort left-half) (mergesort right-half) )])
+    )
+  )
+(mergesort '(1 99 2 -100 3 64 4 5))
+
+; ========================================
+#|
+15.1 write a procedure to binary
+> (to–binary 9)
+1001
+> (to–binary 23)
+10111
+|#
+
+(define (to-binary n)
+  (if (equal? n 0) '(0) (to-binary1 n))
+  )
+
+(define (to-binary1 n)
+   (cond [(zero? n) '()]
+          [else (let* ((q (quotient n 2))
+                      (r (remainder n 2))
+                      )
+                  (se (to-binary1 q) r)
+                  )])
+  )
+(to-binary 0)
+(to-binary 9) ;1001 => 1x8 + 1x4 + 0x2 + 1x1
+(to-binary 23) ;10111 => 1x4 + 1x2 + 1x1
+
+
+; ========================================
+
+#|
+15.2 A "palindrome" is a sentence that reads the same backward as forward. Write a predicate palindrome? that 
+takes a sentence as argument and decides whether it is a palindrome. For example:
+> (palindrome? '(flee to me remote elf))
+#T
+> (palindrome? '(flee to me remote control))
+#F
+Do not reverse any words or sentences in your solution.
+|#
+
+;palindrome?:: sentence -> boolean
+(define (palindrome? sent)
+  (palindrome?-helper (accumulate word sent))
+  )
+
+;palindrome?-helper:: word -> boolean
+(define (palindrome?-helper wd)
+  ; veyr difficult since no reverse alloweed
+  ; flatten the list into words
+  ; check if first xs = last xs
+  ; continue until it reaches the middle
+  (cond [(empty? wd) #t]
+        [(empty? (bf wd)) #t]
+        [else (let* ((fst (first wd))
+                     (lst (last wd))
+                     (drop-fl (bl (bf wd)))
+                     )
+                (cond [(equal? fst lst) (palindrome?-helper drop-fl)] ;drop both first and last
+                      [else #f])
+                )]
+        )
+  )
+; maybe i'/m overcomplicating this?
+(palindrome? '()) ;vacuously true
+(palindrome? '(flee to me remote elf))
+(palindrome? '(ba o ab)) ; should return true
+(palindrome? '(he eh))
+(palindrome? '(flee to me remote control))
+
+; ========================================
