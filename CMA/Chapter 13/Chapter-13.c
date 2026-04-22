2a97bbca2ec2ab4aba9cb7ae140b5083458ab781
Samuel Oktavianus <samueloctavianus97@gmail.com>
2026-04-22 19:30:49 +0700

    update

--- CMA/Chapter 13/Chapter-13.c
+++ CMA/Chapter 13/Chapter-13.c
@@ -1,9 +1,10 @@
 /*
 CHAPTER 13: Strings
 
-String literal
+STRING LITERAL
 "this is a string"
 like racket but iwth \0 to seperate
+we can't modifiy it - read only!
 
 DONT change a string literal
 char *p = "abc"
@@ -13,7 +14,21 @@ SINGLE CHAR STRING LITERAL
 "a" vs 'a'
 
 INITIALIZATION
+just use a[unspeficied] = "STRING" for safety
 
+CHAR VS ARR POINTER
+*date = "June 13" -> cannot be modiifed - string literal!
+date[] = "June 13" -> can be modified - make a copy
+
+ILEGAL
+str1 = "abc"
+str2 = str1
+
+
+'\0' is the same as 0 int
+'\0' is FALSE
+*p = *s2
+if *p = '\0' this is treated as false
 
 */
 
