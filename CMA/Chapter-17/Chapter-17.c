3b9a6a3983c8250e7199f9cefff08f4827ddcc9b
indigoscipio <samueloctavianus97@gmail.com>
2026-05-18 19:57:03 +0700

    update

--- CMA/Chapter-17/Chapter-17.c
+++ CMA/Chapter-17/Chapter-17.c
@@ -6,9 +6,49 @@
 
 
 DYANAMIC STORAGE ALLOCATION
+you can actually make storage during execution
+normally with strings or arrays
 
-malloc returns void * (a generic empty pointer)
+MALLOC
+malloc always returns void * (a generic empty pointer)
 why? coz it has no idea what you're gonna return
+
+void *malloc(size_t size)
+string -> malloc(n+1)
+
+HEAP VS STACK
+heap -> is a block of memory - malloc, realloc and calloc lives here
+stack -> standard variables live here - int, struct, arr, etc - system manages this area automatically
+
+CALLOC
+void * calloc(size_ t nmemb, size_ t size) ;
+like malloc but it cleans the memory first
+takes 2 information: num of items and size of each item
+
+REALLOC
+reallocates either by
+- expanding the room
+- moving to new room
+void *realloc(void *ptr, size_t size);
+1st arg: pointer to memory that you wanna resize (must be created malloc, realloc or calloc)
+2nd arg: new totla size
+
+FREE
+void free(void *ptr)
+just like realloc, this one must take poitner to memory created by alloc, realloc or calloc
+you can also pass NULL
+
+DANGLING POINTER
+so after you do free(p)
+if you try to use p it gives you unexpected behaviour coz its already cleaned
+thats why you have to do p = NULL;
+
+LINKED LIST
+car = first->value
+cdr = first->next
+end of list = '() = NULL
+dont forget to also update first (head pointer) everytime
+
 
 */
 
